4022cb1a34691c5f017d9d3328f11c6961d77b67
Khushal Trivedi <khushalt5@gmail.com>
2020-07-04 17:36:30 +0530

    Feat: working static site for SSP india

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
    return render_template('index.html')
 
 @app.route('/login')
 def login():
 	return render_template('login.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
